5a8b61ca5ab8137fdf7aa9ea71ba63abe86ab427
Yousef Haddad <26yhaddad@overlake.org>
2024-10-28 08:21:57 -0700

    Initial Commit

---
+++ SlopeRacer macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  SlopeRacer macOS
+//
+//  Created by Neven Abou Gazala on 10/28/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ SlopeRacer tvOS/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  SlopeRacer tvOS
+//
+//  Created by Neven Abou Gazala on 10/28/24.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
